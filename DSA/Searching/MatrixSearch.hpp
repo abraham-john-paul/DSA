6482c5e49cd4152ad579291fc1f5dbad995d3114
Abraham Paul <abrahampaul096@gmail.com>
2021-08-22 20:22:07 +0530

    add matrix search and quicksort

--- DSA/Searching/MatrixSearch.hpp
+++ DSA/Searching/MatrixSearch.hpp
@@ -1,51 +1,28 @@
 #include "../header.h"
 
-bool binaryRowSearch(const vector<vector<int> > &A, const int B, int row, int start, int end) {
+int searchMatrix(vector<vector<int> > &A, int B) {
+    int nRow = A.size();
+    int nCol = A[0].size();
+    int r, c;
+    int start = 0;
+    int end = nRow * nCol - 1;
     int mid;
+    
+    int ans = 0;
+    
     while (start <= end) {
         mid = start + (end - start) / 2;
-        if (A[row][mid] == B) {
-            return true;
-        } else if (A[row][mid] > B) {
+        r = mid / nCol;
+        c = mid % nCol;
+        if (A[r][c] == B) {
+            ans = 1;
+            break;
+        } else if (A[r][c] > B) {
             end = mid - 1;
         } else {
             start = mid + 1;
         }
     }
-    return false;
-}
 
-int lowerBoundRow(const vector<vector<int> > &A, const int B) {
-    int start = 0;
-    int end = A.size();
-    int mid;
-    int nCol = A[0].size();
-    while (start <= end) {
-        mid = start + (end - start) / 2;
-        if (A[mid][nCol - 1] >= B) {
-            end = mid;
-        } else {
-            start = mid + 1;
-        }
-    }
-    return start;
-}
-
-int searchMatrix(vector<vector<int> > &A, int B) {
-    int found = 0;
-
-    int nRow = A.size();
-    int nCol = A[0].size();
-    int c = lower_bound(A[nRow -1].begin(), A[nRow - 1].end(), B) - A[nRow - 1].begin();
-    int r = lowerBoundRow(A, B);
-
-    while (r < nRow) {
-        if (A[r][c] > B) {
-            break;
-        }
-        binaryRowSearch(A, B, r, c, nCol - 1);
-        r++;
-    }
-
-    return found;
+    return ans;
 }
--- DSA/Sorting/QuickSort.hpp
+++ DSA/Sorting/QuickSort.hpp
@@ -1,25 +1,29 @@
 #include "../header.h"
+
 int partition(vector<int>& A, int start, int end) {
-    int pivot = A[end];
-    int i = start - 1;
-
-    for (int j = start; j <= (end - 1); j++) {
-        if (A[j] < pivot) {
-            i++;
-            swap(A[i], A[j]);
+    int r = rand() % (end - start + 1) + start;
+    swap(A[r], A[end]);
+    
+    int pIdx = start;
+    int j = start;
+    
+    while (j < end) {
+        if (A[j] < A[end]) {
+            swap(A[j], A[pIdx]);
+            pIdx++;
         }
+        j++;
     }
-
-    swap(A[i + 1], A[end]);
-    return (i + 1);
+    
+    swap(A[pIdx], A[end]);
+    return pIdx;
 }
 
 void quicksort(vector<int>& A, int start, int end) {
     if (start >= end) {
         return;
-    }
-    int pivotIdx = partition(A, start, end);
-    quicksort(A, start, pivotIdx - 1);
-    quicksort(A, pivotIdx + 1, end); 
+    }    
+    int pivotIndex = partition(A, start, end);
+    quicksort(A, start, pivotIndex - 1);
+    quicksort(A, pivotIndex + 1, end);
 }
-
