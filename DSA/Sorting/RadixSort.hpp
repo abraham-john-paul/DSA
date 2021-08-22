764557becdaf8a6828eccbfa5d6cb4d3ec310d0b
Abraham Paul <abrahampaul096@gmail.com>
2021-08-22 20:53:32 +0530

    update radix sort

--- DSA/Sorting/RadixSort.hpp
+++ DSA/Sorting/RadixSort.hpp
@@ -1,21 +1,21 @@
 #include "../header.h"
 
 void radixSort(vector<int>& A) {
-    int maxElem = *max_element(A.begin(), A.end());
     vector<int> buckets[10];
+    int maxElement = *max_element(A.begin(), A.end());
     int digit;
-    int i, j, k;
-    for (i = 1; i <= maxElem; i *= 10) {
-        for (const int e : A) {
+    int j;
+    for (int i = 1; i <= maxElement; i *= 10) {
+        for (const int& e : A) {
             digit = (e / i) % 10;
             buckets[digit].push_back(e);
         }
-        k = 0;
-        for (j = 0; j < 10; j++) {
-            for (const int e : buckets[j]) {
-                A[k++] = e;
+        j = 0;
+        for (auto& bucket : buckets) {
+            for (const int& e : bucket) {
+                A[j++] = e;
             }
-            buckets[j].clear();
+            bucket.clear();
         }
     }
 }
