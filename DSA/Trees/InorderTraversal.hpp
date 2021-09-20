5423e25a9b5e5e865462499d08fdd33226e5abc2
Abraham Paul <abrahampaul096@gmail.com>
2021-09-20 23:50:35 +0530

    problems on trees

---
+++ DSA/Trees/InorderTraversal.hpp
@@ -0,0 +1,38 @@
+#include "../header.h"
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+// void inorder(vector<int>& ans, TreeNode* root) {
+//     if (!root) {
+//         return;
+//     }
+//     inorder(ans, root->left);
+//     ans.push_back(root->val);
+//     inorder(ans, root->right);
+// }
+ 
+ 
+vector<int> Solution::inorderTraversal(TreeNode* A) {
+    stack<TreeNode*> stk;
+    vector<int> ans;
+    auto* node = A;
+    while (!stk.empty() || A) {
+        if (A) {
+            stk.push(A);
+            A = A->left;
+        } else {
+            node = stk.top();
+            stk.pop();
+            ans.push_back(node->val);
+            A = node->right;
+        }
+    }
+    // inorder(ans, A);
+    
+    return ans;
+}
